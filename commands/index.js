4b1f1037594bb3a966c2529862a54b532b2363fa
Yna Ramos <yna.francesca.ramos@gmail.com>
2021-02-26 23:19:11 +0000

    Write test commands

---
+++ commands/index.js
@@ -0,0 +1,26 @@
+const { PREFIX } = require('../config.json');
+
+module.exports = {
+  Nyanpasu: {
+    //require('./nyanpasu'),
+    name: `${PREFIX}nyanpasu`,
+    description: 'Nyanpasu!',
+    execute(msg, args) {
+      msg.channel.send(`Nyanpasu~`);
+    }
+  },
+  Renchon: {
+    name: `${PREFIX}renchon`,
+    description: 'Say Renge\'s nickname!',
+    execute(msg, args) {
+      msg.channel.send('What is it?');
+    }
+  },
+  Server: {
+    name: `${PREFIX}server`,
+    description: 'Display the server name',
+    execute(msg, args) {
+      msg.channel.send(`This server's name is:  ${msg.guild.name}`);
+    }
+  }
+};
---
+++ commands/nyanpasu.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'nyanpasu',
+    description: 'Nyanpasu!',
+    execute(msg, args) {
+      msg.channel.send('Nyanpasu~');
+    },
+};
+  
